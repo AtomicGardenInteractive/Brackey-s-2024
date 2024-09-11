2b9ee74aee16f6cf44d506a26eeebd0750fcb710
Brooke Heathcock <AtomicGardenInteractive@Gmail.com>
2024-09-11 02:05:15 +0100

    Move boat tech

--- Godot/new-game-project/Scenes/Levels/Player.gd
+++ Godot/new-game-project/Scenes/Levels/Player.gd
@@ -3,9 +3,35 @@ extends Camera3D
 @export var unitArray : Array = []
 
 func _input(event):
+#Gets object object under cursor then checks if boat is already in selected units, if it not clear array then adds it to selected units
 	if event.is_action_pressed("Action_Select"):
-		shoot_ray()
+		var hitObject = shoot_ray()
+		if hitObject:
+			if hitObject["collider"] is Boat:
+				var targetBoat: Boat = hitObject["collider"]
+				clear_array()
+				add_to_array(targetBoat)
+			else:
+				pass
+		else: 
+			pass
+#Gets object object under cursor then checks if boat is already in selected units, if it not adds it to selected units
+	if event.is_action_pressed("Action_Queue_Select"):
+		var hitObject = shoot_ray()
+		if hitObject:
+			if hitObject["collider"] is Boat:
+				var targetBoat: Boat = hitObject["collider"]
+				add_to_array(targetBoat)
+			else:
+				pass
+		else: 
+			pass
 
+	if event.is_action_pressed("Action_Command"):
+		#
+		pass
+
+#Gets objects under cursor
 func shoot_ray():
 	var mouse_pos = get_viewport().get_mouse_position()
 	var ray_lenght = 1000
@@ -17,19 +43,24 @@ func shoot_ray():
 	ray_query.to = to
 	var raycast_results = space.intersect_ray(ray_query)
 	print(raycast_results)
-	
-	if raycast_results:
-		if raycast_results["collider"] is Boat:
-			var targetBoat: Boat = raycast_results["collider"]
-			add_to_array(targetBoat)
-		else:
-			pass
-	else: 
-		pass
+	return raycast_results
+
+
+func clear_array():
+	unitArray.clear()
+	pass
 
 func add_to_array(targetBoat:Boat):
-	unitArray.sort()
-	if unitArray.bsearch(targetBoat,true):
+	if search_array(targetBoat) == true:
+		print("boat appended")
 		unitArray.append(targetBoat)
+	else:
+		print("boat already presnent not appended")
 	print(unitArray)
 	pass
+
+func search_array(targetBoat:Boat):
+	if unitArray.count(targetBoat) < 1:
+		return true
+	else:
+		return false
--- Godot/new-game-project/Scenes/Levels/boat.gd
+++ Godot/new-game-project/Scenes/Levels/boat.gd
@@ -2,11 +2,41 @@ extends CharacterBody3D
 class_name Boat
 
 @export var boatID : int = 0
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var boatStyle : Material
+@export var SPEED = 3.0
+
+@onready var nav_agent = $NavigationAgent3D
+
+var targetLocationArray = []
+
+func add_target_location(target_location):
+	targetLocationArray.append(target_location)
+	
+func current_target_location(target_location):
+	nav_agent.target_position = target_location
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func add_to_array(target_location):
+	if search_array(target_location) == true:
+		print("location appended")
+		targetLocationArray.append(target_location)
+	else:
+		print("location already presnent not appended")
+	print(targetLocationArray)
+	pass
+
+func search_array(target_location):
+	if targetLocationArray.count(target_location) < 1:
+		return true
+	else:
+		return false
+
+func _physics_process(delta: float) -> void:
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalize * SPEED
+	
+	velocity = new_velocity
+	move_and_slide()
 	pass
