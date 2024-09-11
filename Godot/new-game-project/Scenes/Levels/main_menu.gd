dff350ab9486f10f85380f1f978c1c7529339a38
Samuel Pascall <samuelpascall18@gmail.com>
2024-09-11 17:02:14 +0100

    Version 2 of Main Menu
    Started coding the new main menu. I hit a road block with the tutorial so it's still not working.

--- Godot/new-game-project/Scenes/Levels/main_menu.gd
+++ Godot/new-game-project/Scenes/Levels/main_menu.gd
@@ -1,16 +1,26 @@
 extends Control
 
+signal start_game()
+@export var buttons_v_box: VBoxContainer = %ButtonsVBox
+
+func _ready() -> void: 
+	focus_buttons()
+
+func _on_start_game_button_pressed() -> void:
+	start_game.emit()
+	hide()
 
 
-func _on_start_game_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/game_handler.tscn")
-
-
-func _on_options_pressed():
-	get_tree().change_scene_to_file("res://options_menu.tscn")
-
-
-func _on_quit_game_pressed():
+func _on_quit_game_button_pressed():
 	get_tree().quit()
-	
-	
+
+
+func _on_visability_changed() -> void:
+	if visible:
+		focus_buttons()
+
+func focus_buttons() -> void: 
+	if buttons_v_box:
+		var button: Button = buttons_v_box.get_child(0)
+		if button is Button:
+				button.grab_click_focus()
